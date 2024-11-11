8883d9a277e61e47d6904e0546459eb4ab5895a9
Swarnab-Code <swarnab2211@gmail.com>
2024-11-12 00:58:24 +0530

    User Order & Status Update Admin & Backend

--- backend/controller/orderController.js
+++ backend/controller/orderController.js
@@ -72,11 +72,33 @@ const verifyOrder = async (req, res) => {
 const userOrders = async (req, res) => {
 	try {
 		const orders = await orderModel.find({userId: req.body.userId})
-		res.json({success: true, orders})
+		res.json({success: true, data: orders})
 	} catch (error) {
 		console.log(error)
 		res.json({success: false, message: "Something went wrong"})
 	}
 }
 
-export { placeOrder, verifyOrder, userOrders }
+// listing orders for admin panel
+const listOrders = async (req, res) => {
+	try {
+		const orders = await orderModel.find({ payment: true })
+		res.json({success: true, data: orders})
+	} catch (error) {
+		console.log(error)
+		res.json({success: false, message: "Something went wrong"})
+	}
+}
+
+// api for updating order status
+const updateStatus = async (req, res) => {
+	try {
+		await orderModel.findByIdAndUpdate(req.body.orderId, {status: req.body.status})
+		res.json({success: true, message: "Order Status Updated"})
+	} catch (error) {
+		console.log(error)
+		res.json({success: false, message: "Something went wrong"})
+	}
+}
+
+export { placeOrder, verifyOrder, userOrders, listOrders, updateStatus }
--- backend/controller/userController.js
+++ backend/controller/userController.js
@@ -58,7 +58,7 @@ const registerUser = async (req, res) => {
 
 		const user = await newUser.save()
 		const token = createToken(user._id)
-		res.json({success: true, token})
+		res.json({success: true, data: {token}})
 	} catch (error) {
 		console.log(error)
 		res.json({success: false, message: "Something went wrong"})
